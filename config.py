1139cf5df8cfb3fc22c7ba1b0e1cf3df23464ffb
Rick Rothbart <richard@noya.co>
2025-05-06 14:29:26 -0700

    set max voltage to 5 v

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ DEFAULT_PORT_SIGGEN = "COM8"
 SIGGEN_BAUD = 115200
 SIGGEN_TIMEOUT = 1.0
 
-PID_DEFAULTS = dict(kp=1.0, ki=0.1, kd=0.05, setpoint=60.0, v_limits=(0.0, 10.0)) 
+PID_DEFAULTS = dict(kp=1.0, ki=0.1, kd=0.05, setpoint=60.0, v_limits=(0.0, 5.0)) 
